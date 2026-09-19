a2e0aa366640ac0611ec3625467fa27d6b2e8928
castleradio <zkzradio@gmail.com>
2026-09-19 18:08:38 +0200

    app

---
+++ sw.js
@@ -0,0 +1,39 @@
+// SKZ Radio - Service Worker para habilitar instalación PWA
+const CACHE_NAME = 'skz-radio-cache-v1';
+const CORE_ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon.svg'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(CORE_ASSETS);
+    }).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+      );
+    }).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  // Las canciones y peticiones dinámicas se procesan directamente por red
+  if (event.request.url.includes('/audio/') || event.request.url.includes('itunes.apple.com')) {
+    return;
+  }
+
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      return cachedResponse || fetch(event.request);
+    })
+  );
+});
