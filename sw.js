a926a817c9d7a19ec479b8effc860d8567af152f
skzradio <zkzradio@gmail.com>
2026-09-22 17:33:49 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // SKZ Radio - Service Worker con actualización automática en segundo plano (Network-First)
-const CACHE_NAME = 'skz-radio-cache-v1.0.3';
+const CACHE_NAME = 'skz-radio-cache-v1.0.5';
 const CORE_ASSETS = [
   './',
   './index.html',
